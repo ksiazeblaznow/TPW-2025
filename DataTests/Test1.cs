7636f020563383fb96ddb4371862ee34b083895c
Michał Olczak <micolo2000@gmail.com>
2025-04-08 00:46:57 +0200

    Added unit tests for Data Layer

---
+++ DataTests/Test1.cs
@@ -0,0 +1,23 @@
+﻿using Data;
+namespace DataTests
+{
+    [TestClass]
+    public class BallTest1
+    {
+        [TestMethod]
+        public void BallCreationTest()
+        {
+
+            float x = 1.0f;
+            float y = 2.0f;
+            float radius = 2.5f;
+
+            Ball ball = new Ball(x, y, radius);
+
+            // Assert
+            Assert.AreEqual(x, ball.PositionX);
+            Assert.AreEqual(y, ball.PositionY);
+            Assert.AreEqual(radius, ball.Radius);
+        }
+    }
+}
