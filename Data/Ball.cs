9a11221c7aa0a00932bd327e9b24d84a15b642e0
ksiazeblaznow <kurchuck.dubbing@gmail.com>
2025-04-08 00:01:40 +0200

    Added class Ball with simple data and Physics empty yet class

---
+++ Data/Ball.cs
@@ -0,0 +1,16 @@
+﻿namespace Data
+{
+    public class Ball
+    {
+        public float PositionX { get; set; }
+        public float PositionY { get; set; }
+        public float Radius { get; set; }
+
+        public Ball(float positionX, float positionY, float radius)
+        {
+            PositionX = positionX;
+            PositionY = positionY;
+            Radius = radius;
+        }
+    }
+}
