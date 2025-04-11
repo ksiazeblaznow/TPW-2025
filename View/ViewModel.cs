7c7742f015aed165ce5b265eaac82c8179fa3ffd
Michał Olczak <micolo2000@gmail.com>
2025-04-11 02:00:45 +0200

    First try of WPF GUI

---
+++ View/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace View
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private int _liczba;
+        public int Liczba
+        {
+            get => _liczba;
+            set
+            {
+                _liczba = value;
+                //OnPropertyChanged();
+            }
+        }
+
+        public ICommand ClickCommand { get; }
+
+        public ViewModel()
+        {
+            ClickCommand = new RelayCommand(OnButtonClick);
+        }
+
+        private void OnButtonClick()
+        {
+            Console.WriteLine($"Wprowadzona liczba: {Liczba}");
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
