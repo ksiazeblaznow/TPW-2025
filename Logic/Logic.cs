94bcdff837d5c1345460b9eaeae236261576e25a
ksiazeblaznow <kurchuck.dubbing@gmail.com>
2025-04-24 12:12:34 +0200

    Create Logic API interface template

---
+++ Logic/Logic.cs
@@ -0,0 +1,12 @@
+﻿using Data;
+
+namespace Logic
+{
+    public class Logic : ILogic
+    {
+        public List<Ball> CreateBall()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Logic/LogicAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Logic
+{
+    public interface ILogic
+    {
+        public List<Ball> CreateBall();
+    }
+}
